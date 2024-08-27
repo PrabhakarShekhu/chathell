086133fad8ed65b7ea74a949ab2592e0de68b989
Prabhakar Shekhu <prabhakarshekhu@gmail.com>
2024-08-27 21:10:16 +0530

    Add build folder

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/chathell/chathell/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.chathell.chathell.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.chathell.chathell.test";
+  public static final String BUILD_TYPE = "debug";
+}
